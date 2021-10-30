5c4d51c88ccf90b8667d693c0ca47e46a9094f19
Martin Zhelev <marty30003@gmail.com>
2021-10-30 17:14:33 +0300

    Scrapy script added

---
+++ collection/collection/spiders/collector.py
@@ -0,0 +1,39 @@
+import scrapy
+import json
+from datetime import date
+
+year = date.today().year
+month = date.today().month
+day = date.today().day - 3
+
+
+class NoticeSpider(scrapy.Spider):
+    name = 'collector'
+    allowed_domains = ['e-licitatie.ro']
+    start_urls = ['http://www.e-licitatie.ro/pub/notices/contract-notices/list/2/1']
+
+    def parse(self, response):
+        url = 'http://www.e-licitatie.ro/api-pub/NoticeCommon/GetCNoticeList/'
+        headers = {'Accept': "application/json, text/plain, */*",
+                   'Content-Type': "application/json;charset=UTF-8",
+                   'Referer': "http://www.e-licitatie.ro/pub/notices/contract-notices/list/2/1",
+                   'Host': 'www.e-licitatie.ro',
+                   'Connection': 'keep-alive',
+                   'Authorization': 'Bearer null',
+                   'Accept-Encoding': "gzip, deflate",
+                   'Cookie': "_HttpSessionID=88FC1C590C1247FC926CBE9669D6A7DF; "
+                             "sysNoticeTypeIds=null; culture=en-US; isCompact=true",
+                   'Origin': "http://www.e-licitatie.ro"}
+        cookies = {"sysNoticeTypeIds": "null", "culture": "en-US", "isCompact": "true"}
+        payload = {"sysNoticeTypeIds": [2], "sortProperties": [], "pageSize": 5000,
+                   "hasUnansweredQuestions": 'false', "startPublicationDate": f"{year}-{month}-{day}T21:00:00.000Z",
+                   "sysProcedureStateId": 'null', "pageIndex": 0,
+                   "endPublicationDate": f"{year}-{month}-{day}T21:00:00.000Z"}
+
+        yield scrapy.Request(url, method="POST", body=json.dumps(payload), headers=headers, cookies=cookies,
+                             callback=self.parse_entry)
+
+    def parse_entry(self, response):
+        items = json.loads(response.text)['items']
+        with open('./data.json', 'w') as file:
+            json.dump(items, file, indent=4)
--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 import json
+import sys
+import os
+
+from collection.collection.spiders.collector import NoticeSpider
+from scrapy.crawler import CrawlerProcess
 from schema_check import return_result
 from database_creation import create_database
-from collect_method import collect
 
-# Make API request, get list
-items = collect()
+# Request through spider
+process = CrawlerProcess()
+process.crawl(NoticeSpider)
+process.start()
 
 # Get needed data
+try:
+    with open('data.json', 'r') as file:
+        items = json.load(file)
+except FileNotFoundError:
+    print("File not found! Terminating...")
+    sys.exit()
+
 valid_cases = {}
 invalid_cases = []
 for key in items:
@@ -23,22 +36,22 @@ for key in items:
             procurement_type = "OFFLINE"
         estimated_value = f"{key['estimatedValueRon']:,.2f}"
         valid_cases[id] = {'date': date, 'number': number, 'name': name, 'procedure_state': procedure_state,
-                    'contract_type': contract_type,
-                    'procurement_type': procurement_type, 'estimated_value': estimated_value}
+                           'contract_type': contract_type,
+                           'procurement_type': procurement_type, 'estimated_value': estimated_value}
     except:
         invalid_cases.append(key)
 
-# Create JSON file for debug purposes
+# Rewrite data JSON with processed data for debug purposes
 createJson = False
 if createJson:
-    with open('debug.json', 'w') as f:
+    with open('data.json', 'w') as f:
         json.dump(valid_cases, f, indent=4)
 
 # Access database after schema validation
 # Check whether there are any entries at all.
 isValid = return_result(valid_cases)
 if isValid:
-    createDatabase = True
+    createDatabase = False
     if createDatabase:
         create_database(valid_cases)
 
@@ -51,3 +64,11 @@ if len(invalid_cases) > 0:
     print("Invalid cases detected:")
     for case in invalid_cases:
         print(case)
+
+# Cleanup of data.json
+cleanup = True
+if cleanup:
+    if os.path.exists("data.json"):
+        os.remove("data.json")
+    else:
+        print("File not found.")
